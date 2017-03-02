30aff77566c17c8ae9b2845dbecf853b4d58bcb5
ozlevka <ozlevka@gmail.com>
2017-03-02 21:19:50 +0200

    Finish creating code for exam

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Scala exam in for recongate
+# Scala exam in for Recongate
 
 1. Write a program in Scala that takes a text file, reverse it and also calculate the 5 most frequent (reversed) words in the text.
 2. The assumption is that the file cannot fit in memory.
--- src/org/ozlevka/exam/LineIterator.scala
+++ src/org/ozlevka/exam/LineIterator.scala
@@ -1,8 +1,11 @@
 package org.ozlevka.exam
 
-import java.io.{File, RandomAccessFile}
+import java.io.{File, FileReader, FileWriter}
+import java.nio.CharBuffer
+import java.nio.file.{Files, Path}
 
 import scala.collection.immutable.HashMap
+import scala.io.Source
 
 /**
   * Created by ozlevka on 3/2/17.
@@ -18,6 +21,8 @@ class LineIterator(it: Iterator[String], dest: File) {
     })
   }
 
+  // Can't be huge amount of words. Even in Shakespeare English exists 800K words then it can be number of millions if we take
+  // all existing words.
   def mergeMap(calculatedMap: HashMap[String, Int]): Unit = {
      this.wordsCount = this.wordsCount ++ calculatedMap.map {
        case (word, count) => word -> (count + this.wordsCount.getOrElse(word, 1))
@@ -29,7 +34,21 @@ class LineIterator(it: Iterator[String], dest: File) {
     sorted.take(5) toMap
   }
 
-  def append(line: String) = {
 
+  //Because files might be huge amount for reverse we need to rewrite destination file each time
+  def append(line: String) = {
+    val tmpFile = new File(dest.getAbsolutePath + "tmp")
+    val output = new FileWriter(tmpFile)
+    if(!dest.exists) {
+       output.write(line)
+    } else {
+       output.write(line)
+       Source.fromFile(dest) getLines() foreach(l => {
+          output.write(l + "\n")
+       })
+       Files.delete(dest.toPath)
+    }
+    output.close()
+    Files.move(tmpFile.toPath, dest.toPath)
   }
 }
